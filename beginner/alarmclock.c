f6aa118743dcaca46931f6a08ed3f57aeb0e1236
meineapfel <gabrielle.plima1965@gmail.com>
2017-12-02 03:38:51 -0200

    Código errado arrumado

--- beginner/alarmclock.c
+++ beginner/alarmclock.c
@@ -3,32 +3,20 @@
 
 int main () 
 {
-	int h1,h2,m1,m2,result;
-	for(;;)
-	{
-		scanf("%d %d %d %d",&h1,&h2,&m1,&m2);
-		if(h1 == 0 && m1 == 0  && h2 == 0 && m2 == 0)
-		{
-			break;
-		}
-	}
-	if(h1 == 0)
-	{
-		h1 = 24;
-	}
-	m1 = h1* 60 + m1;
-	if(h2 == 0) 
-	{
-		h2 = 24;
-	}
-	m2 = h2* 60 + m2;
-	if(m1 >= m2)
-	{
-		m2 = 24* 60 + m2;
-	}
-	result = m2 - m1;
+int h1,h2,m1,m2,result,day_min = 1440;
 
-	printf("%d\n",result);
+scanf("%d %d %d %d",&h1,&m1,&h2,&m2);
+
+m1 += h1*60;
+m2 += h2*60;
+
+if(m2 < m1) {
+result = day_min - m1 + m2;
+}
+else
+result = m2 - m1;
+
+printf("%d",result);
 
 	return 0;
 }
