0561735c1f3485ebe07e0485cda4845b719ae98c
electronicskitten <ethernetcabe@gmail.com>
2017-12-29 12:54:55 -0200

    1010

---
+++ beginner/simplecalculate.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main  () 
+{
+int cod,quant,i;
+double val,total=0;
+for(i=0;i<2;i++) {
+scanf("%i %i %lf",&cod,&quant,&val);
+total += quant*val;
+}
+printf("VALOR A PAGAR: R$ %.2lf\n",total);
+return 0;
+}
+
